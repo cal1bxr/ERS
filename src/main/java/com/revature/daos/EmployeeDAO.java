cc40db8b11f205ec55ffb0a21ef9afef0471e074
Matt Jordan <mjordandevelop@gmail.com>
2021-10-17 00:30:42 -0400

    Fixed error in relationship between tables

--- src/main/java/com/revature/daos/EmployeeDAO.java
+++ src/main/java/com/revature/daos/EmployeeDAO.java
@@ -1,6 +1,8 @@
 package com.revature.daos;
 
+import com.revature.utils.UserNotFoundException;
+
 public interface EmployeeDAO {
-    boolean login(String username, String password);
+    boolean login(String username, String password) throws UserNotFoundException;
 
 }
--- src/main/java/com/revature/daos/EmployeeDAOIMPL.java
+++ src/main/java/com/revature/daos/EmployeeDAOIMPL.java
@@ -1,13 +1,19 @@
 package com.revature.daos;
 
+import com.revature.utils.UserNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class EmployeeDAOIMPL implements EmployeeDAO{
     private static Logger log = LoggerFactory.getLogger(EmployeeDAOIMPL.class);
     @Override
-    public boolean login(String username, String password) {
-        log.info("User logged in");
+    public boolean login(String username, String password) throws UserNotFoundException {
+        try{
+            log.info("User logged in");
+
+        } catch(UserNotFoundException e){
+
+        }
         return false;
     }
 }
